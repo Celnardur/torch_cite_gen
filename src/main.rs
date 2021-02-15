9f414a061700ee3f541ead89e38f5b92754ff274
Celnardur <celnardur@protonmail.com>
2021-02-15 17:10:53 -0500

    Decided that the # would be my unusual function syntax

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,46 @@ use std::path::{PathBuf, Path};
 use std::env;
 use std::fs;
 use std::collections::HashMap;
+use std::error::Error;
 
-#[derive(Debug, Eq, PartialEq, Hash)]
-enum Property {
-    Title,
-    Style,
+#[derive(Debug)]
+struct Function {
+    name: String,
+    args: HashMap<String, String>,
 }
 
-fn main() {
+impl Function {
+    fn parse(block: &str) -> Function {
+        let mut first = true;
+        let mut name = String::new();
+        let mut args = HashMap::new();
+        for line in block.lines() {
+            if let Some((param, arg)) = Function::parse_line(line) {
+                if first {
+                    name = param.clone();
+                    first = false;
+                }
+                args.insert(param, arg);
+            }
+        }
+        Function {
+            name,
+            args,
+        }
+    }
+
+    fn parse_line(line: &str) -> Option<(String, String)> {
+        let first_space = line.find(' ');
+        if line.chars().next() != Some('#') || first_space == None {
+            return None;
+        }
+
+        let (param, arg) = line.split_at(first_space.unwrap());
+        Some((param[1..].to_owned(), arg[1..].to_owned()))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         panic!("Usage: {} src_dir target_dir", args[0]);
@@ -20,43 +52,27 @@ fn main() {
     println!("To: {:?}", src_dir);
     println!("From: {:?}", target_dir);
 
-    let properties_file = src_dir.join("properties.txt");
-    
-    let universal_properties = if let Ok(properties) = fs::read_to_string(&properties_file) {
-        get_properties(&properties)
+    let properties_file = src_dir.join("head.txt");
+    let global_head = if let Ok(properties) = fs::read_to_string(&properties_file) {
+        Some(Function::parse(&properties))
     } else {
-        HashMap::new()
+        None
     };
 
-    println!("{:?}", universal_properties);
+    println!("{:?}", global_head);
 
-    let header = fs::read_to_string(&src_dir.join("header.html")).ok();
-    println!("{:?}", header);
+    let header_file = src_dir.join("header.html");
+    let header = fs::read_to_string(&header_file).ok();
 
-}
-
-fn get_properties(properties_str: &str) -> HashMap<Property, String> {
-    let mut properties = HashMap::new();
-    for line in properties_str.split("\n") {
-        let mut first_space = 0;
-        for (i, c) in line.chars().enumerate() {
-            if c == ' ' {
-                first_space = i
-            }
+    for entry in fs::read_dir(src_dir)? {
+        let path = entry?.path();
+        if path.is_file() && path != properties_file && path != header_file {
+            let target_path = target_dir.join(path.strip_prefix(src_dir)?);
+            fs::copy(path, target_path)?;
+        } else if path.is_dir() {
+            
         }
-        if line.chars().nth(0) != Some('#') || first_space == 0 {
-            continue;
-        }
-
-        let (prop, arg) = line.split_at(first_space);
-        let prop = &prop[1..];
-        let arg = &arg[1..];
-
-        match prop {
-            "title" => properties.insert(Property::Title, arg.to_owned()),
-            "style" => properties.insert(Property::Style, arg.to_owned()),
-            _ => None,
-        };
     }
-    properties
+
+    Ok(())
 }
